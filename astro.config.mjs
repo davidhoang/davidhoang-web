cdd4199bb727a5b145c734864fdc08a81b1e5cd6
David Hoang <dh@Davids-MacBook-Air-3.local>
2024-12-26 12:26:28 -0800

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,6 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    },
-    imageService: true,
-    imagesConfig: {
-      sizes: [640, 750, 828, 1080, 1200, 1920],
-      domains: [],
-      minimumCacheTTL: 60
-    },
-    includeFiles: ['**/*'],
-    excludeFiles: [],
-    debug: true,
-    forceBuild: true
-  })
+  output: 'static',
 }); 
